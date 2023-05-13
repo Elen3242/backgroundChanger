44361de5893440ecefd135d8455dca5b5bdbbb31
Elen3242 <131886011+Elen3242@users.noreply.github.com>
2023-05-13 23:48:05 +0400

    Add files via upload

---
+++ JS homework change background/script/index.js
@@ -0,0 +1,19 @@
+const colors = document.querySelectorAll('.color');
+colors.forEach(box => {
+    box.addEventListener('click', () => {
+        document.body.style.backgroundColor = box.style.backgroundColor;
+    });
+});
+
+// const colors = document.querySelectorAll('.color');
+colors.forEach(box => {
+    box.addEventListener('click', () => {
+        document.body.style.backgroundColor = box.style.backgroundColor;
+    });
+});
+const color = document.querySelectorAll('.color');
+colors.forEach(box => {
+    box.addEventListener('click', () => {
+        document.body.style.backgroundColor = box.style.backgroundColor;
+    });
+});
